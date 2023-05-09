d979872d7695a4433ce37cb1d44703911c068f56
qa-ljhnc-2-miud@example.com <qa-ljhnc-2-miud@example.com>
2023-05-09 16:02:31 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16424.stoplight-dev.com/api/v1/projects/cHJqOjI1MA/spectral.js?branch_slug=main&token=d36bb182-f1d4-4744-a075-807478cc121d";
+export default { extends: ruleset };
